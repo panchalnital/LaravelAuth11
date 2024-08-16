4f3c730a1063759c6449fcec32ad13348bd6d0cd
panchalnital <panchalnital5@gmail.com>
2024-08-16 18:25:17 +0530

    Opean Ai API implements

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,6 +18,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/openai',function(){
+
+    $result = OpenAI::chat()->create([
+        'model' => 'gpt-3.5-turbo',
+        'messages' => [
+            ['role' => 'user', 'content' => 'How to Implement OPen ai api in laravel'],
+        ],
+    ]);
+    
+    echo $result->choices[0]->message->content; // Hello! How can I assist you today?
+
+});
+
 require __DIR__.'/auth.php';
 require __DIR__.'/admin-auth.php';
 require __DIR__.'/doctor-auth.php';
+
