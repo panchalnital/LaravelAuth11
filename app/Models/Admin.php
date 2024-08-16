4b6672284d8acc75b322bb14eedbabee06f42b6b
panchalnital <panchalnital5@gmail.com>
2024-08-16 16:21:18 +0530

    auth laravel projects

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class Admin extends Authenticatable
 {
     use HasFactory, Notifiable;
-    protected $guad='web';
+    protected $guad='admin';
 
     /**
      * The attributes that are mass assignable.
